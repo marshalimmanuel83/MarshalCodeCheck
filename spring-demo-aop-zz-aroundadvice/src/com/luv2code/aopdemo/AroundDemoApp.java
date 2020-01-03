c5f2a7799aaf0475b341a1a6553104d16901363b
Marshal Immanuel <34843530+marshalimmanuel83@users.noreply.github.com>
2020-01-03 09:27:26 +0530

    spring-demo-aop-zz-aroundadvice

---
+++ spring-demo-aop-zz-aroundadvice/src/com/luv2code/aopdemo/AroundDemoApp.java
@@ -0,0 +1,31 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.service.TrafficFortuneService;
+
+public class AroundDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read the spring config Java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from the spring container
+		TrafficFortuneService trafficFortuneService = context.getBean("trafficFortune",TrafficFortuneService.class);
+		
+		System.out.println("\nMain Program : AroundDemoApp");
+		System.out.println("Calling the main method getFortune() from TrafficFortuneService");
+		
+		String data = trafficFortuneService.getFortune();
+		
+		System.out.println("\nYour fortune is : " + data);
+		
+		System.out.println("Main Program : Finished");
+		
+		// close the context
+		context.close();
+
+	}
+
+}
